a53f30f9e89e67a4a289f219d5512de03328e1b1
snowbread <snowbread@163.com>
2024-03-08 15:08:51 +0800

    Initial commit

---
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,11 @@
+package eurekaserver;
+
+// 在eureka-server模块的主类中
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaServerApplication.class, args);
+  }
+}
---
+++ user-authentication-service/src/main/java/com/example/userauthenticationservice/controller/AuthenticationController.java
@@ -0,0 +1,2 @@
+package com.example.userauthenticationservice.controller;public class AuthenticationController {
+}
